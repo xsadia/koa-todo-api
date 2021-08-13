1636c610dcc9d824d72aa052b1654ada11339df0
xsadia <lipearthur@hotmail.com>
2021-08-13 16:47:45 -0300

    added custom context to graphql server

--- src/app.ts
+++ src/app.ts
@@ -8,16 +8,40 @@ import { todoRouter } from './routes/todo.routes';
 import { userRouter } from './routes/users.routes';
 import { schema } from './graphql/schema';
 import { ensureAuthenticatedGraphql } from './middlewares/ensureAuthenticatedGraphql';
+import { getUser } from './middlewares/graphqlAuth';
 
 const app = new Koa();
 const router = new Router();
 
-router.use(ensureAuthenticatedGraphql);
+//router.use(ensureAuthenticatedGraphql);
 
-router.all('/graphql', graphqlHttp({
-    schema,
-    graphiql: true
-}));
+const graphqlSettingsPerReq = async (req) => {
+    const { user } = await getUser(req.header.authorization);
+
+    return {
+        graphiql: true,
+        schema,
+        context: {
+            user,
+            req,
+        },
+        formatError: (error) => {
+            console.log(error.message);
+            console.log(error.locations);
+            console.log(error.stack);
+
+            return {
+                message: error.message,
+                locations: error.locations,
+                stack: error.stack,
+            };
+        },
+    };
+};
+
+const graphqlServer = graphqlHttp(graphqlSettingsPerReq);
+
+router.all('/graphql', graphqlServer);
 
 app.use(bodyParser());
 app.use(convert(cors()));
