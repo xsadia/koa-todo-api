1106f17f957d0410ca95ad1fe1f7a5f478bd0b95
xsadia <lipearthur@hotmail.com>
2021-08-13 21:41:49 -0300

    Added todo queries to QueryType

--- src/graphql/schema/QueryType.ts
+++ src/graphql/schema/QueryType.ts
@@ -1,8 +1,10 @@
-import { GraphQLNonNull, GraphQLObjectType } from "graphql";
+import { GraphQLNonNull, GraphQLObjectType, GraphQLString } from "graphql";
 import { connectionArgs, connectionFromArray } from "graphql-relay";
 import { UserConnection, UserType } from "../user/UserType";
 import * as UserLoader from '../user/UserLoader';
 import { nodeField, nodesField } from "../node/nodeDefinition";
+import { TodoConnection, TodoType } from "../todo/TodoType";
+import * as TodoLoader from '../todo/TodoLoader';
 
 export const QueryType = new GraphQLObjectType({
     name: 'Query',
@@ -11,7 +13,7 @@ export const QueryType = new GraphQLObjectType({
         node: nodeField,
         nodes: nodesField,
         users: {
-            type: GraphQLNonNull(UserConnection),
+            type: new GraphQLNonNull(UserConnection),
             args: connectionArgs,
             resolve: async (root, args, context) => {
                 const data = await UserLoader.loadAll();
@@ -23,11 +25,42 @@ export const QueryType = new GraphQLObjectType({
             resolve: async (root, args, context: any) => {
                 if (context.user) {
                     const data = await UserLoader.loadSingle(context.user._id);
+
                     return data;
                 }
 
                 return;
             },
+        },
+        todos: {
+            type: new GraphQLNonNull(TodoConnection),
+            args: connectionArgs,
+            resolve: async (root, args, context: any) => {
+                if (context.user) {
+                    const data = await TodoLoader.loadAll(context.user._id);
+
+                    return connectionFromArray(data, args);
+                }
+
+                return;
+            }
+        },
+        todo: {
+            type: TodoType,
+            args: {
+                id: {
+                    type: new GraphQLNonNull(GraphQLString)
+                }
+            },
+            resolve: async (root, args, context: any) => {
+                if (context.user) {
+                    const data = await TodoLoader.load(context.user._id, args.id);
+
+                    return data;
+                }
+
+                return;
+            }
         }
     })
 });
