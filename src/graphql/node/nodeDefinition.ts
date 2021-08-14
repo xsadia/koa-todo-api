551d6ad4e3aecdcd547d5838038f1ca250e4c824
xsadia <lipearthur@hotmail.com>
2021-08-13 21:39:44 -0300

    Update nodeDefinition to include TodoType

--- src/graphql/node/nodeDefinition.ts
+++ src/graphql/node/nodeDefinition.ts
@@ -1,5 +1,7 @@
 import { fromGlobalId, nodeDefinitions } from "graphql-relay";
+import { Todo } from "../../models/Todo";
 import { User } from "../../models/User";
+import { TodoType } from "../todo/TodoType";
 import { UserType } from "../user/UserType";
 
 export const { nodeInterface, nodeField, nodesField } = nodeDefinitions(
@@ -9,11 +11,19 @@ export const { nodeInterface, nodeField, nodesField } = nodeDefinitions(
             return await User.findOne({ _id: id });
         }
 
+        if (type === 'Todo') {
+            return await Todo.findOne({ _id: id });
+        }
+
         return null;
     },
     (obj) => {
         if (obj instanceof User) {
             return UserType;
+        }
+
+        if (obj instanceof Todo) {
+            return TodoType;
         }
 
         return null;
