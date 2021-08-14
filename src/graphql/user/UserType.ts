caa441c002611d40b2c014f95de267394b3cd3d6
xsadia <lipearthur@hotmail.com>
2021-08-13 21:44:01 -0300

    updated UserType's todo field to return connection of todos

--- src/graphql/user/UserType.ts
+++ src/graphql/user/UserType.ts
@@ -1,16 +1,14 @@
-import { GraphQLList, GraphQLNonNull, GraphQLObjectType, GraphQLString } from "graphql";
-import { connectionDefinitions, globalIdField } from "graphql-relay";
+import { GraphQLNonNull, GraphQLObjectType, GraphQLString } from "graphql";
+import { connectionArgs, connectionDefinitions, connectionFromArray, globalIdField } from "graphql-relay";
 import { nodeInterface } from "../node/nodeDefinition";
+import { TodoConnection } from "../todo/TodoType";
+import * as TodoLoader from '../todo/TodoLoader';
 
 export const UserType: any = new GraphQLObjectType({
     name: 'User',
     description: 'User type',
     fields: () => ({
         id: globalIdField('User'),
-        /* _id: {
-            type: GraphQLString,
-            resolve: user => user._id
-        }, */
         username: {
             type: new GraphQLNonNull(GraphQLString),
             resolve: user => user.username
@@ -24,8 +22,13 @@ export const UserType: any = new GraphQLObjectType({
             resolve: user => user.password
         },
         todos: {
-            type: new GraphQLNonNull(GraphQLList(GraphQLString)),
-            resolve: user => user.todos
+            type: new GraphQLNonNull(TodoConnection),
+            args: connectionArgs,
+            resolve: async (user, args, context: any) => {
+                const todos = await TodoLoader.loadAll(context.user._id);
+
+                return connectionFromArray(todos, args);
+            }
         }
     }),
     interfaces: () => [nodeInterface]
