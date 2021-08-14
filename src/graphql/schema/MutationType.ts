c021de59eae7229998355b67583cd00bc5b97c10
xsadia <lipearthur@hotmail.com>
2021-08-14 05:19:12 -0300

    add todo mutations

--- src/graphql/schema/MutationType.ts
+++ src/graphql/schema/MutationType.ts
@@ -1,10 +1,12 @@
 import { GraphQLObjectType } from "graphql";
 import UserMutations from '../user/mutations';
+import TodoMutations from '../todo/mutations';
 
 export const MutationType = new GraphQLObjectType({
     name: 'Mutation',
     description: 'Root of all mutation',
     fields: () => ({
-        ...UserMutations
+        ...UserMutations,
+        ...TodoMutations
     })
 });
