af83e402c8a9b5be080a5f5e33dcaa25fe8d75b5
xsadia <felipe.rosa.arthur27@gmail.com>
2022-05-15 12:01:27 -0300

    feat: format

--- src/api/user/createUser.ts
+++ src/api/user/createUser.ts
@@ -1,61 +1,61 @@
-import { User } from '../../models/User';
-import { Context } from 'koa';
-import { hash } from 'bcryptjs';
+import { User } from "../../models/User";
+import { Context } from "koa";
+import { hash } from "bcryptjs";
 
 export const createUser = async (ctx: Context) => {
-    const { email, username, password } = ctx.request.body;
+  const { email, username, password } = ctx.request.body;
 
-    if (!email || !username || !password) {
-        ctx.status = 400;
-        ctx.body = {
-            error: 'E-mail, username and password are required'
-        };
-
-        return;
-    }
-
-    const emailExists = await User.findOne({ email });
-
-    if (emailExists) {
-        ctx.status = 400;
-        ctx.body = {
-            error: 'E-mail already in use'
-        };
-
-        return;
-    }
-
-    const usernameExists = await User.findOne({ username });
-
-    if (usernameExists) {
-        ctx.status = 400;
-        ctx.body = {
-            error: 'Username already in use'
-        };
-
-        return;
-    }
-
-    const hashedPassword = await hash(password, 8);
-
-    const user = new User({
-        email,
-        username,
-        password: hashedPassword
-    });
-
-    await user.save();
-
-    const userWithoutPassword = {
-        _id: user._id,
-        email: user.email,
-        username: user.username,
-    };
-
-    ctx.status = 201;
+  if (!email || !username || !password) {
+    ctx.status = 400;
     ctx.body = {
-        user: userWithoutPassword
+      error: "E-mail, username and password are required",
     };
 
     return;
+  }
+
+  const emailExists = await User.findOne({ email });
+
+  if (emailExists) {
+    ctx.status = 400;
+    ctx.body = {
+      error: "E-mail already in use",
+    };
+
+    return;
+  }
+
+  const usernameExists = await User.findOne({ username });
+
+  if (usernameExists) {
+    ctx.status = 400;
+    ctx.body = {
+      error: "Username already in use",
+    };
+
+    return;
+  }
+
+  const hashedPassword = await hash(password, 8);
+
+  const user = new User({
+    email,
+    username,
+    password: hashedPassword,
+  });
+
+  await user.save();
+
+  const userWithoutPassword = {
+    _id: user._id,
+    email: user.email,
+    username: user.username,
+  };
+
+  ctx.status = 201;
+  ctx.body = {
+    user: userWithoutPassword,
+  };
+
+  return;
 };
--- src/models/Todo.ts
+++ src/models/Todo.ts
@@ -1,31 +1,30 @@
-import { model, Schema, Types, Document } from 'mongoose';
-import { IUser } from './User';
+import { model, Schema, Types, Document } from "mongoose";
+import { IUser } from "./User";
 
 export interface ITodo extends Document {
-    content: string;
-    isCompleted: boolean;
-    createdAt: Date;
-    owner: IUser['_id'];
+  content: string;
+  isCompleted: boolean;
+  createdAt: Date;
+  owner: IUser["_id"];
 }
 
 const TodoSchema = new Schema({
-    content: {
-        type: String,
-        required: true
-    },
-    isCompleted: {
-        type: Boolean,
-        default: false
-    },
-    owner: {
-        type: Types.ObjectId,
-        ref: 'User'
-    },
-    createdAt: {
-        type: Date,
-        default: Date.now
-    }
+  content: {
+    type: String,
+    required: true,
+  },
+  isCompleted: {
+    type: Boolean,
+    default: false,
+  },
+  owner: {
+    type: Types.ObjectId,
+    ref: "User",
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
 });
 
-export const Todo = model<ITodo>('Todo', TodoSchema);
-
+export const Todo = model<ITodo>("Todo", TodoSchema);
